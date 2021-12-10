1a4ded0322abfaa29b75d09b45ac2aa95ff6143f
Vincent Bernat <vincent@bernat.ch>
2021-12-10 14:51:47 +0100

    Initial import

---
+++ home.nix
@@ -0,0 +1,26 @@
+{ config, pkgs, lib, ... }:
+
+{
+  programs.home-manager.enable = true;
+
+  home.username = "bernat";
+  home.homeDirectory = "/home/bernat";
+  home.stateVersion = "21.05";
+
+  home.packages = with pkgs; [
+    bat
+    glibcLocales
+    nix
+    nix-zsh-completions
+    yt-dlp
+  ];
+
+  home.activation.diff = lib.hm.dag.entryBefore ["installPackages"] ''
+    nix store diff-closures "$oldGenPath" "$newGenPath"
+  '';
+}
+
+  # To install:
+  #  - check where nix command is installed (readlink -f =nix)
+  #  - remove everything from nix-env (nix-env -u)
+  #  - install with PATH=/nix/store/...:$PATH nix-shell '<home-manager>' -A install
