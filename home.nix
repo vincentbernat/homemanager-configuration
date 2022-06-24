17ab2b81b4a9e8961e4ee51a39cbaf6a4448659b
Vincent Bernat <vincent@bernat.ch>
2022-06-24 10:00:41 +0200

    Update Firefox

--- home.nix
+++ home.nix
@@ -47,10 +47,10 @@
       };
       firefox = firefox-or-thunderbird rec {
         pname = "firefox";
-        version = "102.0b9";
+        version = "102.0";
         src = pkgs.fetchurl {
-          url = "http://archive.mozilla.org/pub/firefox/releases/${version}/linux-x86_64/en-US/firefox-${version}.tar.bz2";
-          sha256 = "sha256-BcK6IlWdfTW5MZatk70yaMuFlGwQpXgk6vKr4pjj5lA=";
+          url = "http://archive.mozilla.org/pub/firefox/candidates/${version}-candidates/build2/linux-x86_64/en-US/firefox-${version}.tar.bz2";
+          sha256 = "sha256-JnPTh9Iq5uIcIPCR3EgRGXqqUWEQ1EEz5NFMkdVWj4c=";
         };
         genericName = "Web Browser";
         mimeTypes = [
