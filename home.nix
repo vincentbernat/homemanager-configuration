7efba21eabe7eb148a6291b1d70676a131342c3c
Vincent Bernat <vincent@bernat.ch>
2022-11-08 22:46:08 +0100

    Update Thunderbird

--- home.nix
+++ home.nix
@@ -61,10 +61,10 @@
       };
       thunderbird = (firefox-or-thunderbird rec {
         pname = "thunderbird";
-        version = "107.0b3";
+        version = "107.0b4";
         src = pkgs.fetchurl {
           url = "https://download-installer.cdn.mozilla.net/pub/thunderbird/releases/${version}/linux-x86_64/en-US/thunderbird-${version}.tar.bz2";
-          hash = "sha256-ti9fYxm9ztsaWo8ZUGnm1Md2qT0PHGnMTRROyxPfLO4=";
+          hash = "sha256-Efn6UbfsBvy0RoahsB9Mi8Qs0g8Ln7Oc6gu5rXB8mkc=";
         };
         genericName = "Mail Client";
         mimeTypes = [
