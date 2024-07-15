190fd2a6f99d3160952f7a9aac6c0dc940fb2b2e
Vincent Bernat <vincent@bernat.ch>
2024-07-15 07:44:34 +0200

    Switch to Thunderbird 128

--- home.nix
+++ home.nix
@@ -65,7 +65,11 @@
       };
       thunderbird = (firefox-or-thunderbird rec {
         pname = "thunderbird";
-        inherit (pkgs.thunderbird-bin-unwrapped) version src;
+        version = "128.0esr";
+        src = pkgs.fetchurl {
+          url = "mirror://mozilla/thunderbird/releases/${version}/source/thunderbird-${version}.source.tar.xz";
+          sha512 = "8524fbdcc51eddf83fec439273319315c44e6d3be9e4dcf51e453ced7fd1676abdca44442dcb302c637a98b7873828168f2d2d2b635551e406645a134d09aee0";
+        };
         genericName = "Mail Client";
         mimeTypes = [
           "message/rfc822"
