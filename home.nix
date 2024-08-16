f2de842d60501e359f8f4b22a27d7702f5dd7ab1
Vincent Bernat <vincent@bernat.ch>
2024-08-16 10:48:12 +0200

    Tentative to use a patched less
    This does not seem to work, for some reason?

--- home.nix
+++ home.nix
@@ -134,6 +134,15 @@
           })
         ];
       });
+      less = pkgs.less.overrideAttrs (old: {
+        patches = (old.patches or [ ]) ++ [
+          (pkgs.fetchpatch {
+            url = "https://github.com/gwsw/less/commit/1d36fbd0ab607ea7f09ceef8fa6a2da618876b29.patch";
+            hash = "sha256-px9wBz2JL9Q16BPYbbDdANU1YVFTC62yAwVKXqy+glw=";
+            excludes = [ "less.nro.VER" ];
+          })
+        ];
+      });
     in
     with pkgs; [
       bat
@@ -144,6 +153,7 @@
       firefox
       thunderbird
       glibcLocales
+      less
       tmux
       yt-dlp
       flakes.vbeterm.packages.${system}.default
