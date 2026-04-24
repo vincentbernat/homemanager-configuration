503e0ebfae7bfe43abdd7b48d2874fddbcfb98f8
Vincent Bernat <vincent@bernat.ch>
2026-04-24 07:35:50 +0200

    Update claude-code

--- flake.nix
+++ flake.nix
@@ -22,20 +22,10 @@
           name = "nixpkgs-patched";
           src = pkgs.path;
           patches = ([
-            # claude-code 2.1.111
+            # claude-code 2.1.118
             (pkgs.fetchpatch {
-              url = "https://github.com/NixOS/nixpkgs/pull/510655.patch";
-              hash = "sha256-1SnvLrQrEZO1yJ/v+q54ZCxhZXT0ZreqTgZ9BiFsMQ4=";
-            })
-            # claude-code 2.1.112
-            (pkgs.fetchpatch {
-              url = "https://github.com/NixOS/nixpkgs/pull/510736.patch";
-              hash = "sha256-Kal414T04hjDNth2JvsvEabhzeQ4fl+8N8omNw5FWcU=";
-            })
-            # claude-code 2.1.114
-            (pkgs.fetchpatch {
-              url = "https://github.com/NixOS/nixpkgs/pull/511117.patch";
-              hash = "sha256-dm8oXIWNTu94TST0UU/TMl88Qwizqldhu4s/TJBAE5A=";
+              url = "https://github.com/NixOS/nixpkgs/pull/512648.patch";
+              hash = "sha256-WViggOrt5DKSFOpYZLTPLpHS2iYKp8Fc2c7VxHVzP6A=";
             })
           ]);
         })
