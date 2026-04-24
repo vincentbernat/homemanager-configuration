e5f37be409f41fbf0fd91fcae1f7dc6844a83377
Vincent Bernat <vincent@bernat.ch>
2026-04-24 07:36:49 +0200

    Remove nixpkgs patching

--- flake.nix
+++ flake.nix
@@ -16,31 +16,16 @@
     let
       pkgs = (import nixpkgs {
         system = "x86_64-linux";
-      });
-      pkgs-patched = import
-        (pkgs.applyPatches {
-          name = "nixpkgs-patched";
-          src = pkgs.path;
-          patches = ([
-            # claude-code 2.1.118
-            (pkgs.fetchpatch {
-              url = "https://github.com/NixOS/nixpkgs/pull/512648.patch";
-              hash = "sha256-WViggOrt5DKSFOpYZLTPLpHS2iYKp8Fc2c7VxHVzP6A=";
-            })
-          ]);
-        })
-        {
-          inherit (pkgs.stdenv) system;
-          config = {
-            allowUnfreePredicate = pkg: builtins.elem (nixpkgs.lib.getName pkg) [
-              "claude-code"
-            ];
-          };
+        config = {
+          allowUnfreePredicate = pkg: builtins.elem (nixpkgs.lib.getName pkg) [
+            "claude-code"
+          ];
         };
+      });
     in
     {
       homeConfigurations.bernat = inputs.home-manager.lib.homeManagerConfiguration {
-        pkgs = pkgs-patched;
+        inherit pkgs;
         modules = [ ./home.nix ];
         extraSpecialArgs = { flakes = inputs; };
       };
