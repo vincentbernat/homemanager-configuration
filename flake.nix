17871579041e532edb458240434ece11ecb29edf
Vincent Bernat <vincent@bernat.ch>
2026-06-02 09:02:31 +0200

    Revert "Update claude-code"
    This reverts commit 8572f366c4729fb718da851be1d76b7a32b64786.

--- flake.nix
+++ flake.nix
@@ -16,36 +16,16 @@
     let
       pkgs = (import nixpkgs {
         system = "x86_64-linux";
-      });
-      pkgs-patched = import
-        (pkgs.applyPatches {
-          name = "nixpkgs-patched";
-          src = pkgs.path;
-          patches = ([
-            # claude-code 2.1.156
-            (pkgs.fetchpatch {
-              url = "https://github.com/NixOS/nixpkgs/pull/525449.patch";
-              hash = "sha256-m6JNi73wgDmxkXUusSMvMpa0YEy3DhgqcDd6wuosqHA=";
-            })
-            # claude-code 2.1.158
-            (pkgs.fetchpatch {
-              url = "https://github.com/NixOS/nixpkgs/pull/525909.patch";
-              hash = "sha256-dTpKU3QF1bl8WTUJO5eGyLiLeY78Yv8N4LmL8OA8WhQ=";
-            })
-          ]);
-        })
-        {
-          inherit (pkgs.stdenv) system;
-          config = {
-            allowUnfreePredicate = pkg: builtins.elem (nixpkgs.lib.getName pkg) [
-              "claude-code"
-            ];
-          };
+        config = {
+          allowUnfreePredicate = pkg: builtins.elem (nixpkgs.lib.getName pkg) [
+            "claude-code"
+          ];
         };
+      });
     in
     {
       homeConfigurations.bernat = inputs.home-manager.lib.homeManagerConfiguration {
-        pkgs = pkgs-patched;
+        inherit pkgs;
         modules = [ ./home.nix ];
         extraSpecialArgs = { flakes = inputs; };
       };
